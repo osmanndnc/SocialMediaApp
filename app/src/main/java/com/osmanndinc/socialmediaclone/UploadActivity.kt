233d1db72cd9e939187ddb37d77a2ee3c1568236
Osman Nuri Dinç <osmannuri.dinc21@ogr.atauni.edu.tr>
2024-12-08 23:11:29 +0300

    first commit

--- app/src/main/java/com/osmanndinc/socialmediaclone/UploadActivity.kt
+++ app/src/main/java/com/osmanndinc/socialmediaclone/UploadActivity.kt
@@ -1,19 +1,39 @@
 package com.osmanndinc.socialmediaclone
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.registerForActivityResult
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 import com.osmanndinc.socialmediaclone.databinding.ActivityMainBinding
 import com.osmanndinc.socialmediaclone.databinding.ActivityUploadBinding
+import java.util.jar.Manifest
 
 class UploadActivity : AppCompatActivity() {
     private lateinit var binding : ActivityUploadBinding
+    private lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    var selectedPicture : Uri? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -26,12 +46,55 @@ class UploadActivity : AppCompatActivity() {
             insets
 
         }
+        registerLauncher()
     }
 
     fun upload(view: View){
 
     }
     fun selectImage(view: View){
+        if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
 
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Snackbar.make(view,"Permission Needed for gallery",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission"){
+                    //request
+                    permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                }.show()
+            }else{
+                //request
+                permissionLauncher.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+
+        }else{
+            val intentToGallery= Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            activityResultLauncher.launch(intentToGallery)
+
+        }
+
+
+    }
+    private fun registerLauncher(){
+     activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+        if (result.resultCode== RESULT_OK){
+            val intentFromResult= result.data
+            if (intentFromResult != null ){
+                selectedPicture=intentFromResult.data
+                selectedPicture?.let{
+                    binding.imageView.setImageURI(it)
+                }
+
+            }
+        }
+     }
+        permissionLauncher=registerForActivityResult(ActivityResultContracts.RequestPermission()){ result ->
+
+            if (result){
+
+                val intentToGallery= Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+            }else{
+                Toast.makeText(this@UploadActivity,"Permission needed",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
